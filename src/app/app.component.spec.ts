21a252b8fd793974db8686909ec00530ff90a866
KTajerbashi <kamrantajerbashi@gmail.com>
2024-10-08 22:24:41 +0330

    New Project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular_solution' title`, () => {
+  it(`should have the 'angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular_solution');
+    expect(app.title).toEqual('angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular_solution');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular');
   });
 });
