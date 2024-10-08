6d013d3d97c28ce6dd9d9d51332aea9c6b83eb51
KTajerbashi <kamrantajerbashi@gmail.com>
2024-10-09 02:05:42 +0330

    Add Bootstrap and some components

--- Angular.Client/src/app/app.component.ts
+++ Angular.Client/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { UsersComponent } from './component/security/users/users.component';
+import { FooterComponent } from './component/layout/footer/footer.component';
+import { HeaderComponent } from './component/layout/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UsersComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular';
--- Angular.Client/src/app/component/security/users/users.component.ts
+++ Angular.Client/src/app/component/security/users/users.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
 export class UsersComponent {
-
+  firstName: string = 'Kamran';
+  lastName: string = 'Tajerbashi';
+  isActive: boolean = true;
+  currentDate: Date = new Date();
+  toggleActive(): void {
+    this.isActive = !this.isActive;
+  }
 }
