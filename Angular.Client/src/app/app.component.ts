effd863c30d9b1d489c8e0b8bc73680a96b45150
Tajerbashi <kamrantajerbashi@gmail.com>
2024-12-05 21:14:54 +0330

    Fist Lesson

--- Angular.Client/src/app/app.component.ts
+++ Angular.Client/src/app/app.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { CommonModule } from '@angular/common';
+import { ReversPipe } from './pipes/revers.pipe';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatButtonModule,
+    HomeComponent,
+    AboutComponent,
+    CommonModule,
+    ReversPipe,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular.client';
+  /// Pipes
+  title: string = 'Angular Full Course';
+  info: string = 'Angular Version 19';
+  dateTime: Date = new Date();
+  testWords: string[] = [
+    'Kaihan',
+    'Kairun',
+    'sharif',
+    'Javad',
+    'mohammad',
+    'Tajer',
+  ];
+
+  /// Property Binding
+  isDisable = false;
+
+  //  Event Binding
+  eventBindingInput: string = '';
+  changeKeyDown = (e: any) => {
+    this.eventBindingInput = e.target.value;
+  };
+
+  //  Two Way Binding
+  printValue: string = '';
+  printChangeValue = (e: any) => {
+    this.printValue = e.target.value;
+  };
 }
