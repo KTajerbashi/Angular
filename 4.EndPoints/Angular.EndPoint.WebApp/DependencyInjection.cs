e39b32ba3ee269fa986cdd1cbe3457cb2b4639a5
Tajerbashi <kamrantajerbashi@gmail.com>
2025-01-02 17:09:56 +0330

    Feature : Add Web Application and Merge Port

---
+++ 4.EndPoints/Angular.EndPoint.WebApp/DependencyInjection.cs
@@ -0,0 +1,58 @@
+﻿using Angular.EndPoint.WebApi;
+using Microsoft.OpenApi.Models;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddWebAppService(this WebApplicationBuilder builder)
+    {
+        // Add Razor Pages services
+        builder.Services.AddRazorPages();
+
+        return builder;
+    }
+    public static WebApplication UseWebAppPipeline(this WebApplication app)
+    {
+        app.UseWebApiPipeline();
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapStaticAssets();
+        app.MapRazorPages()
+           .WithStaticAssets();
+        return app;
+    }
+}
+
+
+
+
+
+
+#region Web App Default Configuration
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddRazorPages();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapStaticAssets();
+//app.MapRazorPages()
+//   .WithStaticAssets();
+
+//app.Run();
+#endregion
