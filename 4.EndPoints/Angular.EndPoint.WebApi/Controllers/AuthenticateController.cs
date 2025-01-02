e2b4941c3e78356331e1b31429b37bec946ee2b4
Tajerbashi <kamrantajerbashi@gmail.com>
2025-01-02 17:33:56 +0330

    Feature : Add Base Controllers

---
+++ 4.EndPoints/Angular.EndPoint.WebApi/Controllers/AuthenticateController.cs
@@ -0,0 +1,21 @@
+﻿using Angular.EndPoint.WebApi.Controllers.Bases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Angular.EndPoint.WebApi.Controllers;
+
+// Concrete controller with authentication-specific behavior
+public class AuthenticateController : BaseController
+{
+    // Login action simplified without needing async when no I/O operation is involved
+    [HttpPost("Login")]
+    public IActionResult Login()
+    {
+        return Return("Login");
+    }
+
+    [HttpGet("Signout")]
+    public IActionResult Signout()
+    {
+        return Return("Signout");
+    }
+}
---
+++ 4.EndPoints/Angular.EndPoint.WebApi/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using Angular.EndPoint.WebApi.Controllers.Bases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Angular.EndPoint.WebApi.Controllers;
+
+public class AuthorizationController : BaseApiController
+{
+    // Login action simplified without needing async when no I/O operation is involved
+    [HttpPost("Register")]
+    public IActionResult Register()
+    {
+        return Return("Register");
+    }
+
+    [HttpGet("RemoveAccount")]
+    public IActionResult RemoveAccount()
+    {
+        return Return("RemoveAccount");
+    }
+}
