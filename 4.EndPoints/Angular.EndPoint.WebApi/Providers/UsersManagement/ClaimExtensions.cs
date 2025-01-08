34e989f899a6d5587513921462b6c6c82327e57b
KTajerbashi <kamrantajerbashi@gmail.com>
2025-01-08 22:19:14 +0330

    #fix some changes

---
+++ 4.EndPoints/Angular.EndPoint.WebApi/Providers/UsersManagement/ClaimExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace Angular.EndPoint.WebApi.Providers.UsersManagement;
+
+public static class ClaimExtensions
+{
+    public static string GetClaim(this ClaimsPrincipal userClaimsPrincipal, string claimType)
+    {
+        return userClaimsPrincipal.Claims.FirstOrDefault((x) => x.Type == claimType)?.Value;
+    }
+}
