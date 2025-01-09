6ab1d63d6a0497e5e3b42d1ed80961f51feca762
Tajerbashi <kamrantajerbashi@gmail.com>
2025-01-09 10:29:27 +0330

    Sync

--- 2.Application/Angular.ApplicationLibrary/DepenedencyInjection.cs
+++ 2.Application/Angular.ApplicationLibrary/DepenedencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using Angular.ApplicationLibrary.BaseApplication.Pattern.MediatR.Behaviors;
 using MediatR;
-using MediatR.NotificationPublishers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -8,7 +7,7 @@ using System.Reflection;
 namespace Angular.ApplicationLibrary;
 public static class DepenedencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration,List<Assembly> assemblies)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration, List<Assembly> assemblies)
     {
         services.AddScoped<ProviderServices>();
 
