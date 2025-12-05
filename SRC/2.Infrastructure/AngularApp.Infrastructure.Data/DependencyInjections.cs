7d64c96fdd0f1fdea68d0389ff2c03c35e0120ea
KTajerbashi <kamrantajerbashi@gmail.com>
2025-12-05 21:36:46 +0330

    #Feature Add Infrastructure

--- SRC/2.Infrastructure/AngularApp.Infrastructure.Data/DependencyInjections.cs
+++ SRC/2.Infrastructure/AngularApp.Infrastructure.Data/DependencyInjections.cs
@@ -1,6 +1,14 @@
-﻿namespace AngularApp.Infrastructure.Data;
+﻿
+namespace AngularApp.Infrastructure.Data;
 
 public static class DependencyInjections
 {
-
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services, 
+        IConfiguration configuration,
+        params Assembly[] assemblies
+        )
+    {
+        return services;
+    }
 }
