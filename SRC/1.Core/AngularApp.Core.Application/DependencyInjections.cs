5146e661f613e6dcd325ad29435d0bf076c2993d
KTajerbashi <kamrantajerbashi@gmail.com>
2025-12-05 21:36:29 +0330

    #Feature Add Core

--- SRC/1.Core/AngularApp.Core.Application/DependencyInjections.cs
+++ SRC/1.Core/AngularApp.Core.Application/DependencyInjections.cs
@@ -1,6 +1,11 @@
-﻿namespace AngularApp.Core.Application;
+﻿
+
+namespace AngularApp.Core.Application;
 
 public static class DependencyInjections
 {
-
+    public static IServiceCollection AddApplication(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        return services;
+    }
 }
