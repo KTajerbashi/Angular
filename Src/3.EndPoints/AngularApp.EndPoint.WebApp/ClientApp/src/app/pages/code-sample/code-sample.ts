d43ded14fadfa3d4284bf0037cfe7365db790915
KTajerbashi <kamrantajerbashi@gmail.com>
2025-12-12 01:53:36 +0330

    #Feature Add Directive

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/pages/code-sample/code-sample.ts
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/pages/code-sample/code-sample.ts
@@ -1,15 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-code-sample',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './code-sample.html',
   styleUrl: './code-sample.scss',
 })
 export class CodeSample {
+  itemlist: string[] = [];
   message: string = 'Hello, Enterprise Angular!';
   protected readonly title = signal('ClientApp');
   show = () => {
     this.message = 'Message Changed!';
+  };
+
+  add = () => {
+    //  Check not Duplicate
+    if (this.itemlist.includes(this.message)){
+      alert('Duplicate Item!');
+      return;
+    }
+    this.itemlist = [...this.itemlist, this.message];
+  };
+
+  remove = (item: string) => {
+    this.itemlist = this.itemlist.filter((i) => i !== item);
   };
 }
