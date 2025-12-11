7d58f27d4844618c80097dba4e566e5c646a8d3d
KTajerbashi <kamrantajerbashi@gmail.com>
2025-12-11 22:34:24 +0330

    #Feature Add WebApp to AppHost

--- Src/3.EndPoints/AngularApp.AppHost/AppHost.cs
+++ Src/3.EndPoints/AngularApp.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.AngularApp_EndPoint_WebApi>("angularapp-endpoint-webapi");
 
+builder.AddProject<Projects.AngularApp_EndPoint_WebApp>("angularapp-endpoint-webapp");
+
 builder.Build().Run();
