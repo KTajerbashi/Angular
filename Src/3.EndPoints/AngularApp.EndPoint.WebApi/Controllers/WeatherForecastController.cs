374276029d44fabca0b61cc87bac62ed130ab3d2
KTajerbashi <kamrantajerbashi@gmail.com>
2025-12-11 23:45:03 +0330

    #Feature Add Base Dashbaord template

--- Src/3.EndPoints/AngularApp.EndPoint.WebApi/Controllers/WeatherForecastController.cs
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApi/Controllers/WeatherForecastController.cs
@@ -2,9 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AngularApp.EndPoint.WebApi.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController : BaseController
 {
     private static readonly string[] Summaries =
     [
