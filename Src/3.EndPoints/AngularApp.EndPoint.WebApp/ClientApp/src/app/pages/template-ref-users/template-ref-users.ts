74e67fdc2f7246234908317653d70ed68984ae67
KTajerbashi <kamrantajerbashi@gmail.com>
2025-12-17 10:32:49 +0330

    #Fix Some Tags and Elements

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/pages/template-ref-users/template-ref-users.ts
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/pages/template-ref-users/template-ref-users.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-template-ref-users',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './template-ref-users.html',
   styleUrl: './template-ref-users.scss',
 })
