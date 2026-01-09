4ebebbb6accbd0738ea4228384206d5d1b5788be
KTajerbashi <kamrantajerbashi@gmail.com>
2026-01-09 19:05:47 +0330

    #Feature Add Header Logout

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/components/header/header.ts
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/components/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../service/login.service';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss',
 })
 export class Header {
+  /**
+   *
+   */
+  constructor(private _loginService: LoginService) {}
 
+  logout() {
+    this._loginService.Singout();
+  }
 }
