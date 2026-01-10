2d7f68f07a22d1b8a7b41976ee367ab585c1806e
KTajerbashi <kamrantajerbashi@gmail.com>
2026-01-10 21:23:45 +0330

    #Feature Add Reactive Form with new FormGroup and new FormControl

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/page/forms-intro/forms-intro.ts
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/page/forms-intro/forms-intro.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/forms';
 import ILoginDTO from '../../models/ILogin.dto';
 import { CommonModule } from '@angular/common';
+import IRoleDTO from '../../models/IRole.dto';
 
 @Component({
   selector: 'app-forms-intro',
@@ -50,16 +51,35 @@ export class FormsIntro {
   // Template Driven Form Finishid
 
   // Reactive Form
+  roles: IRoleDTO[] = [
+    { Key: 'Admin', Name: 'Admin' },
+    { Key: 'User', Name: 'User' },
+    { Key: 'Customer', Name: 'Customer' },
+    { Key: 'Client', Name: 'Client' },
+  ];
   registerForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
+    // firstName: new FormControl({ value: '', disabled: true }, Validators.required),
+    firstName: new FormControl({ value: 'Kaihan', disabled: false }, Validators.required),
     lastName: new FormControl('', Validators.required),
+    gender: new FormControl('female', Validators.required),
     username: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.email),
-    phone: new FormControl('', [Validators.minLength(10), Validators.maxLength(10)]),
+    email: new FormControl('', [Validators.required,Validators.email]),
+    phone: new FormControl('', [Validators.required,Validators.minLength(10)]),
+    roleKey: new FormControl('User', Validators.required),
+    isRoleDefault: new FormControl(true, [Validators.required]),
   });
   loginHandler() {
     console.log('Form : ', this.registerForm);
+    if (this.registerForm.valid) {
+      alert('Form Is Valid !!!');
+    } else {
+      console.log('Errors : ', this.registerForm.value);
+    }
   }
 
   // Reactive Form Finishid
+
+
+  // FormBuilder Form
+  // FormBuilder Form Finishid
 }
