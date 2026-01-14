27d7b2ed2a3f143113c6f4ee9799e65eff74d32e
KTajerbashi <kamrantajerbashi@gmail.com>
2026-01-14 22:53:38 +0330

    #Feature Add RxJs Subject Type of Subject

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/pages/rxjs-intro/rxjs-intro.ts
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/pages/rxjs-intro/rxjs-intro.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { IZoneModel } from '../../models/ICommon.dto';
 import { CommonModule } from '@angular/common';
-import { concat, merge, Observable, of, tap } from 'rxjs';
+import { concat, delay, filter, map, merge, Observable, of, Subject, tap } from 'rxjs';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-rxjs-intro',
@@ -21,17 +22,61 @@ export class RxjsIntro implements OnInit {
     { title: 'Apple 1', crateDate: new Date('2026/01/01') },
   ];
   zoneData$ = of(this.zoneData);
+
+  age$ = of([14, 11, 34, 12, 53, 23, 54, 32, 43, 65]);
+  _age$ = of(11, 22, 99, -33, 44, 55, 66, -77, 88, 99);
+  __age$ = of(0.13, 0.23, 99, 0.33, 0.43, 0.53);
+
+  subject$ = new Subject();
   ngOnInit(): void {
-    this.zoneData$
-      .pipe(
-        tap((x) => {
-          console.log("Tap : ",x);
-        })
-      )
-      .subscribe({
-        next(value) {
-          console.log('Value : ', value);
-        },
-      });
+    // this.zoneData$
+    //   .pipe(
+    //     tap((x) => {
+    //       console.log('Tap : ', x);
+    //     })
+    //   )w
+    //   .subscribe({
+    //     next(value) {
+    //       console.trace('Value : ', value);
+    //     },
+    //   });
+
+    // this._age$.pipe(map((x) => x * 10)).subscribe((subs) => {
+    //   console.error('Subs : ', subs);
+    // });
+    // concat(this._age$, this.__age$)
+    //   .pipe(map((x) => `${x} - ${x / 2}`))
+    //   .subscribe((item) => {
+    //     console.log('Concat : ', item);
+    //   });
+
+    // merge(this._age$, this.__age$)
+    //   // .pipe(map((x) => `${x} - ${x / 2}`))
+    //   .subscribe((item) => {
+    //     console.log('Merge : ', item);
+    //   });
+    // this._age$
+    //   .pipe(
+    //     // tap((x) => {console.log('tap 1 : ', x, new Date());}),
+    //     delay(3),
+    //     // tap((x) => {console.log('tap 2 : ', x);}),
+    //     map((x) => x * 10),
+    //     // tap((x) => {console.log('tap 3 : ', x);}),
+    //     filter((x) => x > 100)
+    //     // tap((x) => {console.log('tap 4 : ', x);})
+    //   )
+    //   .subscribe((item) => {
+    //     console.error('Sub 1 => ', item);
+    //   });
+
+    this.subject$.subscribe((item) => {
+      console.log('Subject A =>', item);
+    });
+    this.subject$.next(1)
+    this.subject$.next(2)
+    this.subject$.subscribe(item => {
+      console.log('Subject B =>', item);
+    })
+    this.subject$.next(3)
   }
 }
