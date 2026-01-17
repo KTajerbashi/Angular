24ad123ef3dd0b4d65b3e9712971c986e66de94f
KTajerbashi <kamrantajerbashi@gmail.com>
2026-01-17 23:57:30 +0330

    #Feature Add Setting Schema

---
+++ Src/1.Core/AngularApp.Core.Domain/Entities/Setting/AppStateEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace AngularApp.Core.Domain.Entities.Setting;
+
+[Table("AppStates", Schema = "Setting")]
+public class AppStateEntity : AggregateRoot
+{
+    public string Key { get; set; }
+    public string Title { get; set; }
+    public string Schema { get; set; }
+}
+
+
---
+++ Src/1.Core/AngularApp.Core.Domain/Entities/Setting/MenuEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace AngularApp.Core.Domain.Entities.Setting;
+
+[Table("Menus", Schema = "Setting")]
+public class MenuEntity : AggregateRoot
+{
+    [ForeignKey(nameof(MenuEntity))]
+    public long? ParentId { get; set; }
+    public virtual MenuEntity Children { get; set; }
+
+    public string Title { get; set; }
+    public string Schema { get; set; }
+}
+
+
