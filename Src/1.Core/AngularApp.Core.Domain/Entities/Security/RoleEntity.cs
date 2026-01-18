58bb5fe47901dd072c2cdc51817e24c4cb03cd7c
KTajerbashi <kamrantajerbashi@gmail.com>
2026-01-18 20:46:39 +0330

    #Feature Add Domain Entity

--- Src/1.Core/AngularApp.Core.Domain/Entities/Security/RoleEntity.cs
+++ Src/1.Core/AngularApp.Core.Domain/Entities/Security/RoleEntity.cs
@@ -1,7 +1,7 @@
 ﻿namespace AngularApp.Core.Domain.Entities.Security;
 
 [Table("Roles", Schema = "Security")]
-public class RoleEntity : IdentityRole<long>, IAuditableEntity<long>
+public class RoleEntity : IdentityRole<long>, IAggregate<long>
 {
     public bool IsDeleted { get; set; }
 
@@ -9,11 +9,23 @@ public class RoleEntity : IdentityRole<long>, IAuditableEntity<long>
 
     public Guid EntityId { get; set; }
 
+    /// <summary>
+    /// لیست 
+    /// Event
+    /// های مربوطه را نگهداری می‌کند        
+    /// </summary>
+    private readonly List<IDomainEvent> _events;
+    protected RoleEntity() => _events = new();
+    protected void AddEvent(IDomainEvent @event) => _events.Add(@event);
+    public void ClearEvents() => _events.Clear();
+    public IEnumerable<IDomainEvent> GetEvents() => _events.AsEnumerable();
+
     public void Delete()
     {
         IsActive = false;
         IsDeleted = true;
     }
+
 
     public void Restore()
     {
--- Src/1.Core/AngularApp.Core.Domain/Entities/Security/UserEntity.cs
+++ Src/1.Core/AngularApp.Core.Domain/Entities/Security/UserEntity.cs
@@ -1,7 +1,8 @@
-﻿namespace AngularApp.Core.Domain.Entities.Security;
+﻿
+namespace AngularApp.Core.Domain.Entities.Security;
 
 [Table("Users", Schema = "Security")]
-public class UserEntity : IdentityUser<long>, IAuditableEntity<long>
+public class UserEntity : IdentityUser<long>, IAggregate<long>
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
@@ -13,11 +14,23 @@ public class UserEntity : IdentityUser<long>, IAuditableEntity<long>
 
     public Guid EntityId { get; set; }
 
+    /// <summary>
+    /// لیست 
+    /// Event
+    /// های مربوطه را نگهداری می‌کند        
+    /// </summary>
+    private readonly List<IDomainEvent> _events;
+    protected UserEntity() => _events = new();
+    protected void AddEvent(IDomainEvent @event) => _events.Add(@event);
+    public void ClearEvents() => _events.Clear();
+    public IEnumerable<IDomainEvent> GetEvents() => _events.AsEnumerable();
+
     public void Delete()
     {
         IsActive = false;
         IsDeleted = true;
     }
+
 
     public void Restore()
     {
