01697123621567e05b13948de77039bc7e68a5bb
KTajerbashi <kamrantajerbashi@gmail.com>
2026-01-20 22:08:29 +0330

    #Feature Add NGRX To Project

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/app.config.ts
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptors/token-interceptor';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
