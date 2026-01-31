5709fa4b7d12e2b2919864a659b9fb3c215ef066
KTajerbashi <kamrantajerbashi@gmail.com>
2026-02-01 00:52:09 +0330

    #Feature Add Layouts

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/layouts/main-layout/main-layout.ts
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/layouts/main-layout/main-layout.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../../shared/components/header/header.component";
+import { AsideComponent } from "../../shared/components/aside/aside.component";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, AsideComponent],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
