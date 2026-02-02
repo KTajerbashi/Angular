fc9ce1663c09209803d7f4741a6b0d1096c5c3d1
KTajerbashi <kamrantajerbashi@gmail.com>
2026-02-02 23:37:57 +0330

    #Feature Add Login Page

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/features/auth/pages/login/login.component.ts
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/features/auth/pages/login/login.component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'component-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  model = {
+    username: '',
+    password: '',
+    rememberMe: false,
+  };
 
+  login() {
+    console.log('Login model:', this.model);
+    // TODO: call AuthService.login()
+  }
+
+  loginWithGoogle() {
+    console.log('Login with Google');
+    // TODO: redirect to Google OAuth
+  }
+
+  loginWithGitHub() {
+    console.log('Login with GitHub');
+    // TODO: redirect to GitHub OAuth
+  }
+
+  loginWithSSO() {
+    console.log('Login with SSO');
+    // TODO: redirect to SSO endpoint
+  }
 }
