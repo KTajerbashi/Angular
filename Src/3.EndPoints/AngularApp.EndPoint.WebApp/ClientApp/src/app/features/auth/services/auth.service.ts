723d9b27f191b90f7b2e552a3afa7f8b5efca626
KTajerbashi <kamrantajerbashi@gmail.com>
2026-02-04 00:25:29 +0330

    #Feature Add Base Services and Api Service in Client

---
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { BaseApiService } from '../../../core/services/base-api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService extends BaseApiService{
+
+}
