2830cfdbe6cde2432b2cc824528bdd598d08b312
KTajerbashi <kamrantajerbashi@gmail.com>
2026-03-21 15:48:33 +0330

    #Feature Add DTOs

--- Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/core/models/IJsonResult.d.ts
+++
@@ -1,7 +0,0 @@
-interface IJsonResult<TModel> {
-  data: TModel;
-  message: string;
-  error: string;
-  token: string;
-  isSuccess: boolean;
-}
---
+++ Src/3.EndPoints/AngularApp.EndPoint.WebApp/ClientApp/src/app/core/models/dtos/IChartTreeDTO.d.ts
@@ -0,0 +1,1 @@
+interface IChartTreeDTO extends IBaseTree<IChartTreeDTO> {}
